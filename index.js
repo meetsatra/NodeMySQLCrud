27fe5260e8ed4cb8330786c2a06a4fcc967e4e7e
Meet Satra <meetvsatra@gmail.com>
2023-07-06 13:19:09 +0530

    GET POST DELETE

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 const connection = require('./connection');
 const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.json())
+// app.use(bodyParser.json())
+
+app.get('/employees',(req,res)=>{
+    // http://localhost:3000/employees
+    connection.query('SELECT * FROM employee',(err,rows)=>{
+        if(err){
+            console.log(err);
+        }else{
+            // console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.get('/employees/:id',(req,res)=>{
+    // http://localhost:3000/employees/1
+    connection.query('SELECT * FROM employee WHERE id=?',[req.params.id],(err,rows)=>{
+        if(err){
+            console.log(err);
+        }else{
+            // console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.delete('/employees/:id',(req,res)=>{
+    // http://localhost:3000/employees/1
+    connection.query('DELETE FROM employee WHERE id=?',[req.params.id],(err,rows)=>{
+        if(err){
+            console.log(err);
+        }else{
+            // console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post('/employees',(req,res)=>{
+    // http://localhost:3000/employees/
+    var emp = req.body
+    console.log(emp);
+    var empData = [emp.name,emp.salary]
+    connection.query('INSERT INTO employee(name,salary) VALUES(?)',[empData],(err,rows)=>{
+        if(err){
+            console.log(err);
+        }else{
+            // console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.listen(3000,()=>console.log('Express server is running on port:3000'))
